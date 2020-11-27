d6ea1943ea65715d9f186463ce32f8decd4836ab
noicepollution <ncvineeth97@gmail.com>
2020-11-27 17:02:32 +0530

    Pushed solutions for 011, 012, 013

---
+++ codes/012HighlyTriangular.py
@@ -0,0 +1,24 @@
+# Project Euler Solutions: Problem 012
+# Copyright (c) noicepollution. All Rights Reserved.
+# Solution timestamp: 15:23, 02 December 2016
+# https://github.com/noicepollution/project-euler
+
+# Beginning the search at 28 as it is already given in the question.
+# Generate a new triangular number with each iteration.
+# For each triangular number, calculate the number of divisors.
+# If the number of divisors exceeds 500, break out of the loop.
+
+n = 28
+i = 8
+while True:
+	n = n+i
+	i = i+1
+	c = 2
+	j = 2
+	while(j*j<=n):
+		if n%j==0:
+			c = c+2
+		j = j+1
+	if c>500:
+		print(n,c)
+		break
