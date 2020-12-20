651ad7fe5c89415ad425ccdb7439095fc13a83fa
noicepollution <ncvineeth97@gmail.com>
2020-12-20 15:27:52 +0530

    Pushed solutions for 046, 047

---
+++ codes/046GoldbackConj2.py
@@ -0,0 +1,32 @@
+# Project Euler Solutions: Problem 046
+# Copyright (c) noicepollution. All Rights Reserved.
+# Solution timestamp: 20:17, 15 March 2020
+# https://github.com/noicepollution/project-euler
+
+from math import sqrt, floor, ceil
+
+def check(x):
+	i = 2
+	while i<=x//i:
+		if x%i==0:
+			return False
+		i = i+1
+	return True
+
+i = 7
+notFound = True
+while notFound:
+	i += 2
+	if not check(i):
+		possible = False
+		j = 2
+		while j < i:
+			if check(j):
+				res = sqrt((i - j) / 2)
+				if floor(res) == ceil(res):
+					possible = True
+					break
+			j = j+2 if j != 2 else j+1
+		if possible == False:
+			notFound = False
+print(i)
