0f174e1d6a1e2ea17b9d42ec4a19741ca6c304cb
noicepollution <ncvineeth97@gmail.com>
2020-12-27 23:02:11 +0530

    Pushed solutions for 058, 059

---
+++ codes/059XORDecryp.py
@@ -0,0 +1,20 @@
+# Project Euler Solutions: Problem 059
+# Copyright (c) noicepollution. All Rights Reserved.
+# Solution timestamp: 00:39, 18 March 2020
+# https://github.com/noicepollution/project-euler
+
+from statistics import mode
+
+inp_str = open('problem59.txt').read().split(',')
+inp_str = [int(i) for i in inp_str]
+lsts = []
+for i in range(0,3):
+	lst = []
+	for j in range(i, len(inp_str), 3):
+		lst.append(inp_str[j])
+	lsts.append(lst)
+
+freqs = [mode(i) for i in lsts]
+key = [i ^ 32 for i in freqs]
+res = sum([inp_str[i] ^ key[i%3] for i in range(0, len(inp_str))])
+print(res)
