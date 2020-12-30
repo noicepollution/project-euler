18f3672d519b93b788411b5c7bd3b5f655266f86
noicepollution <ncvineeth97@gmail.com>
2020-12-30 15:43:50 +0530

    Pushed solutions for 069, 071, 072

---
+++ codes/069Totient.py
@@ -0,0 +1,18 @@
+# Project Euler Solutions: Problem 069
+# Copyright (c) noicepollution. All Rights Reserved.
+# Solution timestamp: 20:40, 09 June 2017
+# https://github.com/noicepollution/project-euler
+
+import math
+def checkprime(x):
+	for i in range(2,x):
+		if x%i==0:
+			return False
+	return True
+res = 2
+i = 3
+while res*i<1000000:
+	if checkprime(i):
+		res =  res*i
+	i = i+1
+print(res)
